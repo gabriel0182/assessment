856776186df4e27a48fd80e0ceb43ffff7f2f37c
gabrielgomezQA <gabriel.gomez@perficient.com>
2022-09-01 09:44:14 -0500

    assessment done

--- test/cypress/e2e/test.spec.js
+++ test/cypress/e2e/test.spec.js
@@ -1,14 +1,19 @@
-
 import firstTest from '../support/pageObject/firstTest'
 
 before(() => {
   cy.visit('https://google.com')
-  });
+})
 
-describe("go to google", () => {
-  
-  it("search something", () => {
-    firstTest.search()
-  });
+describe('go to google', () => {
+  it('search something by testlio', () => {
+    firstTest.search('testlio')
+  })
 
-});
+  it('I select and Print the 3rd results Login', () => {
+    firstTest.selectResultLogin()
+  })
+
+  it('I select and Print the 3rd results Become a Tester', () => {
+    firstTest.selectResultBecome()
+  })
+})
--- test/cypress/support/pageObject/firstTest.js
+++ test/cypress/support/pageObject/firstTest.js
@@ -1,10 +1,37 @@
 class firstTest {
+  static search(val) {
+    cy.intercept('POST', 'https://play.google.com/log?**').as('search')
+    cy.get('input').first().should('be.visible').clear().type(`${val}{enter}`)
+    cy.wait('@search')
+  }
 
-static search(){
-    cy.get('input').first().should('be.visible')
-    .clear().type('something{enter}')
-}
+  static selectResultLogin() {
+    cy.get('#search').within(() => {
+      cy.get('tbody')
+        .children('tr')
+        .children('td:nth-child(1)')
+        .find('a')
+        .first()
+        .then(($a) => {
+          const login = $a.text()
+          cy.log(login)
+        })
+    })
+  }
 
+  static selectResultBecome() {
+    cy.get('#search').within(() => {
+      cy.get('tbody')
+        .children('tr')
+        .children('td:nth-child(1)')
+        .find('a')
+        .eq(1)
+        .then(($a) => {
+          const login = $a.text()
+          cy.log(login)
+        })
+    })
+  }
 }
 
 export default firstTest
